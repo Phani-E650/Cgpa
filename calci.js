0efa2db51c2fcb55b22147389ca28b78ab6354ce
Phani Kumar <58501017+PhaniChowdary-E6@users.noreply.github.com>
2020-06-10 09:54:06 +0530

    updated

--- calci.js
+++ calci.js
@@ -12,6 +12,8 @@ total_subject=(subject1+subject2+subject3+subject4+subject5+subject6)*3;
 total_lab=(lab1+lab2)*2;
 total1=total_subject+total_lab;
 total=total1/22;
+percentage=(total-0.7)*10;
 //console.log(total);
 document.getElementById("calci").innerHTML = "The sgpa is :" + total;
+document.getElementById("calci1").innerHTML = "The percentage is :" + percentage +"%";
 }
